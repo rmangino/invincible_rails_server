54772c2d0c600c34317b676d5a29b6b736718cef
Reed Mangino <reed@themanginos.com>
2015-07-11 09:47:08 -0400

    Add bin directory and command-line app script

---
+++ spec/invincible_rails_server/invincible_rails_server_spec.rb
@@ -0,0 +1,12 @@
+require 'invincible_rails_server'
+require 'spec_helper'
+
+describe InvincibleRailsServer do
+  it 'has a version number' do
+    expect(InvincibleRailsServer::VERSION).not_to be nil
+  end
+
+  # xit 'does something useful' do
+  #   expect(false).to eq(true)
+  # end
+end
